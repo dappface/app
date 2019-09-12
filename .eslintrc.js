dc96058d275f141c1c91bce1e97d39a4b60e0647
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-09-12 15:44:02 +0200

    setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ]
 }

103e0dd6ebba073fb298b63a55d5b876150a99da
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-09-12 16:11:35 +0200

    use Buffer.from instead of new Buffer

--- src/utils/image-util.ts
+++ src/utils/image-util.ts
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export async function fetchImageSource(url: string): Promise<string> {
   const res = await axios.get(url, {responseType: 'arraybuffer'})
-  const base64Image = new Buffer(res.data, 'binary').toString('base64')
+  const base64Image = Buffer.from(res.data, 'binary').toString('base64')
   return `data:image/png;base64,${base64Image}`
 }

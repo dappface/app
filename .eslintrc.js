eafe781a6a7849fffaea7676329121e689c73e1f
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-09-12 16:30:20 +0200

    allow absolute path

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,25 @@ module.exports = {
     'airbnb-typescript',
     'prettier',
     'prettier/@typescript-eslint',
-    'prettier/react'
+    'prettier/react',
   ],
   rules: {
     'import/prefer-default-export': 'off',
     'import/no-extraneous-dependencies': [
       'error',
       {
-        'devDependencies': false,
-        'optionalDependencies': false,
-        'peerDependencies': false,
-        'packageDir': './'
-      }],
-  }
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: false,
+        packageDir: './',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['./'],
+      },
+    },
+  },
 }

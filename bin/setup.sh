7a6f846656ed72cfaf22756a48b25c946d507b0a
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-09-27 22:59:55 +0200

    init google cloud SDK

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,6 +13,8 @@ if [ "${GITHUB_WORKFLOW:-}" == 'CI' ]; then
   curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$GOOGLE_CLOUD_SDK_VERSION-darwin-x86_64.tar.gz"
   tar -xzf "./google-cloud-sdk-$GOOGLE_CLOUD_SDK_VERSION-darwin-x86_64.tar.gz"
   ./google-cloud-sdk/install.sh
+  ./google-cloud-sdk/bin/gcloud init
+  gcloud kms -h
 
   # Authenticate with gcloud
 fi

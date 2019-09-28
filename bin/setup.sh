778b967c79cf1a5459e2e8c99c06082aaca9e4b2
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-09-28 13:49:54 +0200

    fix path

--- bin/setup.sh
+++ bin/setup.sh
@@ -13,7 +13,7 @@ if "${IS_MACOS:-false}" ; then
   tar -xzf "./google-cloud-sdk-$GOOGLE_CLOUD_SDK_VERSION-darwin-x86_64.tar.gz"
   CLOUDSDK_CORE_DISABLE_PROMPTS=1 ./google-cloud-sdk/install.sh
 
-  echo ::add-path::./google-cloud-sdk/bin/gcloud
+  echo ::add-path::./google-cloud-sdk/bin
 
   echo "$GOOGLE_CLOUD_KEY" | base64 --decode > "$HOME"/google-cloud-key.json
   gcloud auth activate-service-account --key-file="$HOME"/google-cloud-key.json

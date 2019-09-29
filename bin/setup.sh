ed12a7028593c38418c160f4e8f632ec8d818d28
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-09-29 23:40:00 +0200

    try

--- bin/setup.sh
+++ bin/setup.sh
@@ -8,15 +8,18 @@ GOOGLE_CLOUD_SDK_VERSION='264.0.0'
 
 if "${IS_MACOS:-false}" ; then
   brew install imagemagick
+  sudo xcode-select -s /Applications/Xcode_11.app
 fi
 
 if [ ! -z "${GOOGLE_CLOUD_KEY:-}" ] ; then
   curl -O "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-$GOOGLE_CLOUD_SDK_VERSION-darwin-x86_64.tar.gz"
   tar -xzf "./google-cloud-sdk-$GOOGLE_CLOUD_SDK_VERSION-darwin-x86_64.tar.gz"
   CLOUDSDK_CORE_DISABLE_PROMPTS=1 ./google-cloud-sdk/install.sh
+
   echo ::add-path::./google-cloud-sdk/bin
 
-  sudo xcode-select -s /Applications/Xcode_11.app
+  echo "$GOOGLE_CLOUD_KEY" | base64 --decode > "$HOME"/google-cloud-key.json
+  gcloud auth activate-service-account --key-file="$HOME"/google-cloud-key.json
 fi
 
 npm i

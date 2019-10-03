f940b7f2fdd0256081825e7aac28d5299bd28b4c
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-10-03 20:13:27 +0200

    remvoe rnn configuratino for iOS

--- ios/DappFace/AppDelegate.m
+++ ios/DappFace/AppDelegate.m
@@ -7,12 +7,12 @@
 
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
 #import <Firebase.h>
 #import "Orientation.h"
-#import <ReactNativeNavigation/ReactNativeNavigation.h>
 #import "RNSplashScreen.h"
 
 @implementation AppDelegate
@@ -20,9 +20,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   [FIRApp configure];
-  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-  [ReactNativeNavigation bootstrap:jsCodeLocation launchOptions:launchOptions];
-
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"DappFace"
+                                            initialProperties:nil];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  [RNSplashScreen show];
   return YES;
 }
 

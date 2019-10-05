26bffa6bb8dd24e03d91746668fd90a85f15ea1b
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-10-05 18:28:21 +0200

    fix generate mnemonic length

--- src/utils/random-bytes.ts
+++ src/utils/random-bytes.ts
@@ -1,6 +1,6 @@
 import {randomBytes} from 'react-native-randombytes'
 
-export function randomBytesAsync(length: number = 32): Promise<Uint8Array> {
+export function randomBytesAsync(length: number = 16): Promise<Uint8Array> {
   return new Promise((resolve, reject): void => {
     randomBytes(length, (error, bytes): void => {
       if (error) {

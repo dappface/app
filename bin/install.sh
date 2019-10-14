8a83f3fa844417e750e58cd2de19b9f271765724
Luke Sugiura <jun.sugiura.jp@gmail.com>
2019-10-14 16:54:41 +0200

    add clean option to install all

--- bin/install.sh
+++ bin/install.sh
@@ -4,6 +4,31 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
+while test $# -gt 0; do
+  case "$1" in
+    --clean)
+      shift
+
+      watchman watch-del-all
+      rm -rf node_modules
+      (
+        cd ios
+        rm -rf Pods
+      )
+
+      rm -rf $TMPDIR/react-native-packager-cache-*
+      rm -rf $TMPDIR/metro-bundler-cache-*
+      rm -rf $TMPDIR/react*
+      rm -rf $TMPDIR/metro*
+      rm -rf $TMPDIR/haste-*
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+
 npm i
 bundle install
 (
